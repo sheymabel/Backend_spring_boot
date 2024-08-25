46fe057c879fcdab86618ededba6b83cd81b3514
sheymabel <122784710+sheymabel@users.noreply.github.com>
2024-08-25 21:25:07 +0100

    final copie

--- testproject/src/main/java/com/Repository/AgantOrangeRepository.java
+++ testproject/src/main/java/com/Repository/AgantOrangeRepository.java
@@ -1,12 +1,13 @@
 package com.Repository;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.entites.Agant_orange ;
-  public interface AgantOrangeRepository  extends JpaRepository <Agant_orange , Long>{
+  public interface AgantOrangeRepository  extends JpaRepository <Agant_orange , Integer>{
     List<Agant_orange> findByName(String name);
 
     // Find agents by status
@@ -15,5 +16,9 @@ import com.entites.Agant_orange ;
     // Custom query using JPQL
     @Query("SELECT a FROM Agant_orange a WHERE a.name LIKE %:name%")
     List<Agant_orange> searchByName(@Param("name") String name);
+
+    void deleteById(Long id);
+
+    public Optional<Agant_orange> findById(Long id);
 
 }
